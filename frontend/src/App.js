445cbad1e24fee737402fa7da027761c3f451cc4
Anna Prokopeva <46624560+heyanny2@users.noreply.github.com>
2023-10-19 22:02:15 +0200

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home.jsx';
+import Autorization from './pages/Autorization.jsx';
+import NotFound from './pages/NotFound.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Autorization />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );  
 }
 
 export default App;
---
+++ frontend/src/pages/Autorization.jsx
@@ -0,0 +1,5 @@
+const Autorization = () => {
+  
+}
+
+export default Autorization;
